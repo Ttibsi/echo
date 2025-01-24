7b685ede2aa8f9589d9109c5945f611ef506e4ce
ttibsi <ashisbitt@icloud.com>
2025-01-24 22:15:16 +0000

    working final echo server! :D

--- echo.asm
+++ echo.asm
@@ -9,6 +9,7 @@ SYS_SENDTO = 44
 SYS_RECVFROM = 45
 SYS_BIND = 49
 SYS_LISTEN = 50
+SYS_SETSOCKOPT = 54
 SYS_EXIT = 60
 
 STDIN = 0
@@ -27,8 +28,10 @@ macro exit returncode {
 }
 
 macro empty_buffer [buf] {
-    mov rax, buf
-    stosq
+    mov edi, buffer
+    mov ecx, 256
+    xor eax, eax
+    rep stosb
 }
 
 macro str_cmp buf, str {
@@ -38,12 +41,6 @@ macro str_cmp buf, str {
 
     cmps dword [fs:esi],[edi]
     setnz al
-}
-
-macro run_call {
-    syscall
-    cmp rax,0
-    jl .error
 }
 
 macro close_socket fd {
@@ -57,7 +54,7 @@ _start:
     mov rdi, AF_INET
     mov rsi, SOCK_STREAM
     mov rdx, 0
-    run_call
+    syscall
 
     mov r12, rax ; server fd
 
@@ -69,19 +66,19 @@ _start:
     mov rdi, r12
     mov rsi, addr.sin_family
     mov rdx, addr_size
-    run_call
+    syscall
 
     mov rax, SYS_LISTEN
     mov rdi, r12
     mov rsi, 8
-    run_call
+    syscall
 
 .loop:
     mov rax, SYS_ACCEPT
     mov rdi, r12
     mov rsi, 0
     mov rdx, 0
-    run_call
+    syscall
 
     mov r13, rax ; client fd
 
@@ -92,7 +89,7 @@ _start:
     mov r10, 0
     mov r8, 0
     mov r9, 0
-    run_call
+    syscall
 
     mov rax, SYS_WRITE
     mov rdi, STDOUT
@@ -107,49 +104,36 @@ _start:
     mov r10, 0
     mov r8, 0
     mov r9, 0
-    run_call
+    syscall
 
     str_cmp buffer, quit_str
     test rax, rax
     jz .quit
 
     empty_buffer buffer
-
     close_socket r13
-    jne .loop
+    jmp .loop
 
 .quit:
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    mov rsi, goodbye
+    mov rdx, goodbye_size
+    syscall
+
     mov rax, SYS_SENDTO
     mov rdi, r13
     mov rsi, goodbye
-    mov rdx, $-goodbye
+    mov rdx, goodbye_size
     mov r10, 0
     mov r8, 0
     mov r9, 0
-    run_call
+    syscall
 
     close_socket r13
-
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, goodbye
-    mov rdx, $-goodbye
-    run_call
-
     close_socket r12
     exit 0 
     ret
-
-.error:
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, error_msg
-    mov rdx, 7
-    syscall
-
-    close_socket r12
-    close_socket r13
-    exit 1
 
 segment readable writeable
 struc servaddr_in {
@@ -160,7 +144,8 @@ struc servaddr_in {
 }
 
 quit_str db "quit", 10, 0
-goodbye db "Goodbye...", 10, 0
+goodbye db 27, "[36mGoodbye...", 27, "[0m", 10, 0
+goodbye_size = $ - goodbye
 error_msg db "Error", 10, 0
 
 buffer db 256 dup 0
